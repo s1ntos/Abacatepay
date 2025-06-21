d55612a8371059d3e7a14907454c170eb7894c94
João <109487647+s1ntos@users.noreply.github.com>
2025-06-21 15:06:29 -0300

    Subindo projeto na main

---
+++ src/main/java/com/api/abacatepay/AbacatepayApplication.java
@@ -0,0 +1,13 @@
+package com.api.abacatepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacatepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacatepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/abacatepay/AbacatepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.abacatepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacatepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
